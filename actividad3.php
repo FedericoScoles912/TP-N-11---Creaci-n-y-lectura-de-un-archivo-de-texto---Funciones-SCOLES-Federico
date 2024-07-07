4f9db10ec8888c28a7828aa5784078d5b5061fb5
FedericoScoles912 <163576246+FedericoScoles912@users.noreply.github.com>
2024-07-07 14:27:51 -0300

    Update actividad3.php

--- actividad3.php
+++ actividad3.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Resultado de Suscripción</title>
+    <title>Problema</title>
 </head>
 <body>
-<?php
-echo "Nombre y Apellido: " . $_REQUEST['nombre'] . "<br>";
-echo "Edad: " . $_REQUEST['edad'] . "<br>";
-echo "DNI: " . $_REQUEST['dni'] . "<br>";
-echo "Fecha de Nacimiento: " . $_REQUEST['fecha_nacimiento'] . "<br>";
-echo "Género: " . $_REQUEST['genero'] . "<br>";
-echo "Lugar de Nacimiento: " . $_REQUEST['lugar_nacimiento'] . "<br>";
-echo "Dirección: " . $_REQUEST['direccion'] . "<br>";
-echo "Intereses y/o Hobbies: " . $_REQUEST['intereses'] . "<br>";
-echo "Comentarios: " . $_REQUEST['comentarios'] . "<br>";
-?>
+    <?php
+    function cuadradocubo($valor, &$cuad, &$cub) {
+        $cuad = $valor * $valor;
+        $cub = $valor * $valor * $valor;
+    }
+
+    cuadradocubo(2, $c1, $c2);
+    echo "El cuadrado de 2 es: " . $c1 . "<br>";
+    echo "El cubo de 2 es: " . $c2;
+    ?>
 </body>
 </html>
