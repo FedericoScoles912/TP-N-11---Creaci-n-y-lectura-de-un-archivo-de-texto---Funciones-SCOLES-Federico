3ae58af9cd7e28f3731598aea7f41a7b977c97d6
FedericoScoles912 <163576246+FedericoScoles912@users.noreply.github.com>
2024-07-07 14:29:34 -0300

    Update actividad5.php

--- actividad5.php
+++ actividad5.php
@@ -1,19 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Resultado de Suscripción</title>
+    <title>Mostrar Pedidos</title>
 </head>
 <body>
-<?php
-echo "Nombre y Apellido: " . $_REQUEST['nombre'] . "<br>";
-echo "Edad: " . $_REQUEST['edad'] . "<br>";
-echo "DNI: " . $_REQUEST['dni'] . "<br>";
-echo "Fecha de Nacimiento: " . $_REQUEST['fecha_nacimiento'] . "<br>";
-echo "Género: " . $_REQUEST['genero'] . "<br>";
-echo "Lugar de Nacimiento: " . $_REQUEST['lugar_nacimiento'] . "<br>";
-echo "Dirección: " . $_REQUEST['direccion'] . "<br>";
-echo "Intereses y/o Hobbies: " . $_REQUEST['intereses'] . "<br>";
-echo "Comentarios: " . $_REQUEST['comentarios'] . "<br>";
-?>
+    <?php
+    $ar = fopen("pedidos.txt", "r") or die("No se pudo abrir el archivo");
+    while (!feof($ar)) {
+        $linea = fgets($ar);
+        $lineasalto = nl2br($linea);
+        echo $lineasalto;
+    }
+    fclose($ar);
+    ?>
 </body>
 </html>
