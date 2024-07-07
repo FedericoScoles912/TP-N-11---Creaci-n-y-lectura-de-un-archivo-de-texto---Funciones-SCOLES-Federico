72f164cfd70bc96096063eeb2caf6bfd56cdbcd0
FedericoScoles912 <163576246+FedericoScoles912@users.noreply.github.com>
2024-07-07 14:36:51 -0300

    Update actividad9.php

--- actividad9.php
+++ actividad9.php
@@ -1,19 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Resultado de Suscripción</title>
+    <title>Confirmación de Inscripción</title>
 </head>
 <body>
-<?php
-echo "Nombre y Apellido: " . $_REQUEST['nombre'] . "<br>";
-echo "Edad: " . $_REQUEST['edad'] . "<br>";
-echo "DNI: " . $_REQUEST['dni'] . "<br>";
-echo "Fecha de Nacimiento: " . $_REQUEST['fecha_nacimiento'] . "<br>";
-echo "Género: " . $_REQUEST['genero'] . "<br>";
-echo "Lugar de Nacimiento: " . $_REQUEST['lugar_nacimiento'] . "<br>";
-echo "Dirección: " . $_REQUEST['direccion'] . "<br>";
-echo "Intereses y/o Hobbies: " . $_REQUEST['intereses'] . "<br>";
-echo "Comentarios: " . $_REQUEST['comentarios'] . "<br>";
-?>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nombre = $_REQUEST['nombre'];
+        $dni = $_REQUEST['dni'];
+        $fecha_nacimiento = $_REQUEST['fecha_nacimiento'];
+        $curso_actual = $_REQUEST['curso_actual'];
+        $materias_adeudadas = $_REQUEST['materias_adeudadas'];
+        $materia_inscripcion = $_REQUEST['materia_inscripcion'];
+        $fecha_inscripcion = $_REQUEST['fecha_inscripcion'];
+        
+        $ar = fopen("comprobante.txt", "a") or die("Problemas en la creación del archivo");
+        fputs($ar, "Apellidos y Nombres: $nombre\n");
+        fputs($ar, "DNI: $dni\n");
+        fputs($ar, "Fecha de Nacimiento: $fecha_nacimiento\n");
+        fputs($ar, "Curso Actual: $curso_actual\n");
+        fputs($ar, "Cantidad de Materias adeudadas: $materias_adeudadas\n");
+        fputs($ar, "Materia y Curso de Inscripción: $materia_inscripcion\n");
+        fputs($ar, "Fecha de Inscripción: $fecha_inscripcion\n");
+        fputs($ar, "--------------------------------------------------------\n");
+        fclose($ar);
+        
+        echo "La inscripción se realizó correctamente.";
+    } else {
+        echo "Por favor complete el formulario.";
+    }
+    ?>
 </body>
 </html>
